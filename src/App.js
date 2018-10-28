2f77d95522b8d3bd16e12bc46c8484584f8f9222
Kurt Smith <kurt.smith@safetyculture.io>
2018-10-28 21:51:43 +1000

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    const username = prompt("What's your name?", "");
+
+    const socket = new WebSocket(`ws://${window.location.host}/ws`);
+    socket.addEventListener('message', this.receiveMessage);
+
+    this.state = {
+      username,
+      inputText: "",
+      messages: [],
+      socket
+    };
+  }
+
+  receiveMessage = (e) => {
+    const message = JSON.parse(e.data);
+    console.log(message);
+    this.setState({ messages: [...this.state.messages, message] });
+  }
+
+  sendMessage = () => {
+    const {username, inputText: message, socket} = this.state;
+    const serialised = JSON.stringify({ username, message });
+    console.log(`Sending: ${serialised}`);
+    socket.send(serialised);
+    console.log(socket);
+    this.setState({ inputText: "" });
+  };
+
+  handleInputChange = ({currentTarget}) => {
+    this.setState({ inputText: currentTarget.value });
+  };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="message-history">
+          {this.state.messages.map(message => {
+            return (
+              <div className="message">
+                <div className="username">{message.username}</div>
+                <div>{message.message}</div>
+              </div>
+            );
+          })}
+        </div>
+        <div className="chat-input">
+          <input
+            placeholder="Type your message here..."
+            value={this.state.inputText}
+            onChange={this.handleInputChange}
+            onSubmit={this.sendMessage}
+          />
+          <button className="primary-button" onClick={this.sendMessage}>
+            Send
+          </button>
+        </div>
       </div>
     );
   }
